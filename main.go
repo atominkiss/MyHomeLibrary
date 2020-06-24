19d65b8cd695af593086e923728b0fde9d02aca6
atomink <atomink@gmail.com>
2020-06-24 12:17:56 +0300

    ++ funcion: creating book; get list of books; get one book.

--- main.go
+++ main.go
@@ -1,16 +1,51 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
 type Book struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Author  *Author `json:"author"`
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
 }
 
 type Author struct {
-	Firstname   string  `json:"firstname"`
-	Lastname    string  `json:"lastname"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
 
+var books []Book
+
 func main() {
-	
+
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
