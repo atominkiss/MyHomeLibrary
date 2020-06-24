6edb4dfe251f94e7434403de8e26374e230330ef
atomink <atomink@gmail.com>
2020-06-24 13:25:00 +0300

    ++ main function with routing. tested by Postman. All works fine.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -22,7 +23,29 @@ type Author struct {
 var books []Book
 
 func main() {
-
+	r := mux.NewRouter()
+	books = append(books, Book{
+		ID:    "1",
+		Title: "Война и Мир",
+		Author: &Author{
+			Firstname: "Lev",
+			Lastname:  "Tolstoy",
+		},
+	})
+	books = append(books, Book{
+		ID:    "2",
+		Title: "Crime and Punishment",
+		Author: &Author{
+			Firstname: "Fedor",
+			Lastname:  "Dostoevsky",
+		},
+	})
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
